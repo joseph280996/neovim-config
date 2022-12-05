d487f3e7e3058b04ebfc279a2a1190ee4f6b977a
Tung Pham <tpham2@ezesoft.com>
2022-12-05 11:46:47 -0500

    Some modification

---
+++ lua/user/lsp/settings/tsserver.lua
@@ -0,0 +1,34 @@
+return {
+    settings = {
+        javascript = {
+            inlayHints = {
+                includeInlayEnumMemberValueHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayVariableTypeHints = true,
+            },
+            format = {
+                semicolons = 'insert',
+                insertSpaceAfterFunctionKeywordForAnonymousFunctions = true,
+            }
+        }
+        typescript = {
+            inlayHints = {
+                includeInlayEnumMemberValueHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayVariableTypeHints = true,
+            },
+            format = {
+                semicolons = 'insert',
+                insertSpaceAfterFunctionKeywordForAnonymousFunctions = true,
+            }
+        },
+    },
+}
