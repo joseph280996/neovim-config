cbcd40bae871506f41bf0183f41f6ea326876392
Tung Pham <joseph.pham280996@gmail.com>
2023-06-11 09:37:28 -0400

    Fold config change

--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -38,31 +38,6 @@ local util = require("lspconfig/util")
 
 local path = util.path
 
-local function get_python_path(workspace)
-	-- Use activated virtualenv.
-	if vim.env.VIRTUAL_ENV then
-		return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
-	end
-
-	-- Find and use virtualenv from pipenv in workspace directory.
-	-- local match = vim.fn.glob(path.join(workspace, 'Pipfile'))
-	-- if match ~= '' then
-	--   local venv = vim.fn.trim(vim.fn.system('PIPENV_PIPFILE=' .. match .. ' pipenv --venv'))
-	--   return path.join(venv, 'bin', 'python')
-	--end
-
-	-- Find and use virtualenv in workspace directory.
-  for _, pattern in ipairs({'*', '.*'}) do
-    local match = vim.fn.glob(path.join(workspace, pattern, 'pyvenv.cfg'))
-    if match ~= '' then
-      return path.join(path.dirname(match), 'bin', 'python')
-    end
-  end
-
-	-- Fallback to system Python.
-	return vim.fn.exepath("python3") or vim.fn.exepath("python") or "python"
-end
-
 local opts = {}
 local language_specific_opts = {}
 
@@ -77,15 +52,6 @@ for _, server in pairs(servers) do
 	if server == "tsserver" then
 		language_specific_opts = {
 			filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-		}
-		opts = vim.tbl_deep_extend("keep", language_specific_opts, opts)
-	end
-
-	if server == "pyright" then
-		language_specific_opts = {
-			before_init = function(_, config)
-				config.settings.python.pythonPath = get_python_path(config.root_dir)
-			end,
 		}
 		opts = vim.tbl_deep_extend("keep", language_specific_opts, opts)
 	end
