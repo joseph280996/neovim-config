60e4790f90017fa7c6f1189fb42837808ee02c7f
Tung Pham <joseph.pham280996@gmail.com>
2023-07-06 14:26:06 -0400

    Update sesison plugin

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -1,11 +1,27 @@
 -- Session
 return {
-  "rmagatti/auto-session",
-  opts = {
-    log_level = "error",
-    auto_session_suppress_dirs = { "~/", "~/Downloads", "~/Code/Project", "~/Work", "~/Code/Research" },
-    auto_session_enable_last_session = false,
-    auto_save_enabled = true,
-    auto_session_enabled = true,
-  }
+  -- Session
+  {
+    "rmagatti/session-lens",
+    dependencies = {
+      "rmagatti/auto-session",
+      "nvim-telescope/telescope.nvim",
+    },
+  },
+  {
+    "rmagatti/auto-session",
+    opts = {
+      log_level = "error",
+      auto_session_suppress_dirs = {
+        "~/",
+        "~/Downloads",
+        "~/Code/Project",
+        "~/Work",
+        "~/Code/Research",
+      },
+      auto_session_enable_last_session = false,
+      auto_save_enabled = true,
+      auto_session_enabled = true,
+    },
+  },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,114 +1,108 @@
-local icons = require "user.icons"
+local icons = require("user.icons")
 
 return {
-    -- Telescope
-    {
-        "nvim-telescope/telescope.nvim",
-        branch = "0.1.x",
-        dependencies = { { "nvim-lua/plenary.nvim" } },
-        opts = {
-            defaults = {
-                prompt_prefix = icons.ui.Telescope .. " ",
-                selection_caret = "  ",
-                path_display = { "smart" },
-                file_ignore_patterns = {
-                    ".git/",
-                    "docs/",
-                    "vendor/",
-                    "__pycache__/",
-                    "node_modules/",
-                    ".idea/",
-                    ".settings/",
-                    ".vscode/",
-                    "build/",
-                    "env/",
-                    ".idea/",
-                    "gradle/",
-                    "%.dll",
-                    "%.exe",
-                    "%.ipynb",
-                },
-            },
-            pickers = {
-                find_files = {
-                    theme = "dropdown",
-                },
-                live_grep = {
-                    --@usage don't include the filename in the search results
-                    only_sort_text = true,
-                    theme = "dropdown",
-                },
-                grep_string = {
-                    only_sort_text = true,
-                    theme = "dropdown",
-                },
-                buffers = {
-                    theme = "dropdown",
-                    previewer = false,
-                    initial_mode = "normal",
-                },
-                planets = {
-                    show_pluto = true,
-                    show_moon = true,
-                },
-                git_files = {
-                    theme = "dropdown",
-                    hidden = true,
-                    previewer = false,
-                    show_untracked = true,
-                },
-                lsp_references = {
-                    theme = "dropdown",
-                    initial_mode = "normal",
-                },
-                lsp_definitions = {
-                    theme = "dropdown",
-                    initial_mode = "normal",
-                },
-                lsp_declarations = {
-                    theme = "dropdown",
-                    initial_mode = "normal",
-                },
-                lsp_implementations = {
-                    theme = "dropdown",
-                    initial_mode = "normal",
-                },
-            },
-            extensions = {
-                fzf = {
-                    fuzzy = true,
-                    override_generic_sorter = true,
-                    override_file_sorter = true,
-                    case_mode = "smart_case"
-                }
-            }
+  -- Telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { { "nvim-lua/plenary.nvim" } },
+    opts = {
+      defaults = {
+        prompt_prefix = icons.ui.Telescope .. " ",
+        selection_caret = "  ",
+        path_display = { "smart" },
+        file_ignore_patterns = {
+          ".git/",
+          "docs/",
+          "vendor/",
+          "__pycache__/",
+          "node_modules/",
+          ".idea/",
+          ".settings/",
+          ".vscode/",
+          "build/",
+          "env/",
+          ".idea/",
+          "gradle/",
+          "%.dll",
+          "%.exe",
+          "%.ipynb",
         },
-    },
-    -- Session
-    {
-        "rmagatti/session-lens",
-        dependencies = {
-            "rmagatti/auto-session",
-            "nvim-telescope/telescope.nvim",
+      },
+      pickers = {
+        find_files = {
+          theme = "dropdown",
         },
-        config = function()
-            require('telescope').load_extension('session_lens')
-        end
+        live_grep = {
+          --@usage don't include the filename in the search results
+          only_sort_text = true,
+          theme = "dropdown",
+        },
+        grep_string = {
+          only_sort_text = true,
+          theme = "dropdown",
+        },
+        buffers = {
+          theme = "dropdown",
+          previewer = false,
+          initial_mode = "normal",
+        },
+        planets = {
+          show_pluto = true,
+          show_moon = true,
+        },
+        git_files = {
+          theme = "dropdown",
+          hidden = true,
+          previewer = false,
+          show_untracked = true,
+        },
+        lsp_references = {
+          theme = "dropdown",
+          initial_mode = "normal",
+        },
+        lsp_definitions = {
+          theme = "dropdown",
+          initial_mode = "normal",
+        },
+        lsp_declarations = {
+          theme = "dropdown",
+          initial_mode = "normal",
+        },
+        lsp_implementations = {
+          theme = "dropdown",
+          initial_mode = "normal",
+        },
+      },
+      extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        },
+      },
     },
-    {
-        "nvim-telescope/telescope-media-files.nvim",
-        config = function()
-            require('telescope').load_extension('media_files')
-        end
-    },
-    -- Fuzzy Finder
-    {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build =
-        "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-        config = function()
-            require('telescope').load_extension('fzf')
-        end
-    },
-    "tom-anders/telescope-vim-bookmarks.nvim",
+    config = function(_, opts)
+      require('telescope').setup(opts)
+
+      -- Because session.lua is loaded before telescope, we need 
+      -- this here so that the loading order is correct
+      require("telescope").load_extension("session-lens")
+    end,
+  },
+  {
+    "nvim-telescope/telescope-media-files.nvim",
+    config = function()
+      require("telescope").load_extension("media_files")
+    end,
+  },
+  -- Fuzzy Finder
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+    config = function()
+      require("telescope").load_extension("fzf")
+    end,
+  },
 }
