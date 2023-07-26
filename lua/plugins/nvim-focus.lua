fa387190e5f6d95ae3c88f4494f28cfc7b34deca
Tung Pham <joseph.pham280996@gmail.com>
2023-07-26 12:19:14 -0400

    Add disabling of focus for certain filetype and buftype

--- lua/plugins/nvim-focus.lua
+++ lua/plugins/nvim-focus.lua
@@ -1,15 +1,40 @@
+local ignore_filetypes = {
+  "lazy",
+  "NvimTree",
+  "help",
+  "neotest-summary",
+  "TelescopePrompt",
+  "alpha",
+}
+
+local ignore_buftypes = { "nofile", "prompt", "popup" }
+
 return {
   "nvim-focus/focus.nvim",
   version = "*",
-  opts = {
-    excluded_filetypes = {
-      "lazy",
-      "NvimTree",
-      "toggleterm",
-      "help",
-      "neotest-summary",
-      "TelescopePrompt",
-      "alpha"
-    }
-  }
+  config = function()
+    require("focus").setup()
+
+    local augroup = vim.api.nvim_create_augroup("FocusDisable", { clear = true })
+
+    vim.api.nvim_create_autocmd( "WinEnter" , {
+      group = augroup,
+      desc = "Disable focus autoresize for BufType",
+      callback = function(_)
+        if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
+          vim.b.focus_disable = true
+        end
+      end,
+    })
+
+    vim.api.nvim_create_autocmd( "FileType", {
+      group = augroup,
+      desc = "Disable focus autoresize for FileType",
+      callback = function(_)
+        if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+          vim.b.focus_disable = true
+        end
+      end,
+    })
+  end,
 }
