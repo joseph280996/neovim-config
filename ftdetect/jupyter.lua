9495f42b941034d244f5f59786876de28be076f1
Tung Pham <joseph.pham280996@gmail.com>
2024-10-20 19:07:49 -0400

    feat!: update AI and minimal change on file detection

--- ftdetect/jupyter.lua
+++ ftdetect/jupyter.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_create_autocmd({'BufRead', 'BufNew'}, {
-  pattern = '*.ipynb',
-  command = 'setfiletype ipynb'
+vim.api.nvim_create_autocmd({ "VimEnter", "BufRead", "BufNew" }, {
+  pattern = "*.ipynb",
+  command = "setfiletype ipynb",
 })
--- lua/ai/avante.lua
+++ lua/ai/avante.lua
@@ -1,91 +1,91 @@
-local get_values_on_os = require("utils.get-values-on-os")
+-- local get_values_on_os = require("utils.get-values-on-os")
 return {
-  "yetone/avante.nvim",
-  lazy = false,
-  version = false, -- set this if you want to always pull the latest change
-  opts = {
-    behavior = {
-      auto_set_highlight_group = false,
-    },
-    mappings = {
-      diff = {
-        ours = "<leader>uaco",
-        theirs = "<leader>uact",
-        all_theirs = "<leader>uaca",
-        cursor = "<leader>uacc",
-        next = "<leader>ua]x",
-        prev = "<leader>ua[x",
-        both = "<leader>uacb",
-      },
-      suggestion = {
-        accept = "<leader>uasl",
-        next = "<leader>uas]",
-        prev = "<leader>uas[",
-        dismiss = "<leader>uasd",
-      },
-      jump = {
-        next = "<leader>uaj]",
-        prev = "<leader>uaj[",
-      },
-      submit = {
-        normal = "<CR>",
-        insert = "<C-s>",
-      },
-    },
-  },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = get_values_on_os({
-    Linux = "make",
-    Window = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false",
-  }, true),
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "stevearc/dressing.nvim",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    "nvim-tree/nvim-web-devicons",
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = get_values_on_os({ Linux = false, Window = true }),
-        },
-      },
-    },
-  },
-  keys = {
-    {
-      "<leader>uaa",
-      "<cmd>AvanteAsk<cr>",
-      desc = "AI Ask",
-      silent = true,
-      noremap = true,
-      nowait = true,
-    },
-    {
-      "<leader>uar",
-      "<cmd>AvanteRefresh<cr>",
-      desc = "AI Refresh",
-      silent = true,
-      noremap = true,
-      nowait = true,
-    },
-    {
-      "<leader>uae",
-      "<cmd>AvanteEdit<cr>",
-      desc = "AI Edit prompt",
-      silent = true,
-      noremap = true,
-      nowait = true,
-    },
-  },
+--   "yetone/avante.nvim",
+--   lazy = false,
+--   version = false, -- set this if you want to always pull the latest change
+--   opts = {
+--     behavior = {
+--       auto_set_highlight_group = false,
+--     },
+--     mappings = {
+--       diff = {
+--         ours = "<leader>uaco",
+--         theirs = "<leader>uact",
+--         all_theirs = "<leader>uaca",
+--         cursor = "<leader>uacc",
+--         next = "<leader>ua]x",
+--         prev = "<leader>ua[x",
+--         both = "<leader>uacb",
+--       },
+--       suggestion = {
+--         accept = "<leader>uasl",
+--         next = "<leader>uas]",
+--         prev = "<leader>uas[",
+--         dismiss = "<leader>uasd",
+--       },
+--       jump = {
+--         next = "<leader>uaj]",
+--         prev = "<leader>uaj[",
+--       },
+--       submit = {
+--         normal = "<CR>",
+--         insert = "<C-s>",
+--       },
+--     },
+--   },
+--   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+--   build = get_values_on_os({
+--     Linux = "make",
+--     Window = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false",
+--   }, true),
+--   dependencies = {
+--     "nvim-treesitter/nvim-treesitter",
+--     "stevearc/dressing.nvim",
+--     "nvim-lua/plenary.nvim",
+--     "MunifTanjim/nui.nvim",
+--     "nvim-tree/nvim-web-devicons",
+--     {
+--       -- support for image pasting
+--       "HakonHarnes/img-clip.nvim",
+--       event = "VeryLazy",
+--       opts = {
+--         -- recommended settings
+--         default = {
+--           embed_image_as_base64 = false,
+--           prompt_for_file_name = false,
+--           drag_and_drop = {
+--             insert_mode = true,
+--           },
+--           -- required for Windows users
+--           use_absolute_path = get_values_on_os({ Linux = false, Window = true }),
+--         },
+--       },
+--     },
+--   },
+--   keys = {
+--     {
+--       "<leader>uaa",
+--       "<cmd>AvanteAsk<cr>",
+--       desc = "AI Ask",
+--       silent = true,
+--       noremap = true,
+--       nowait = true,
+--     },
+--     {
+--       "<leader>uar",
+--       "<cmd>AvanteRefresh<cr>",
+--       desc = "AI Refresh",
+--       silent = true,
+--       noremap = true,
+--       nowait = true,
+--     },
+--     {
+--       "<leader>uae",
+--       "<cmd>AvanteEdit<cr>",
+--       desc = "AI Edit prompt",
+--       silent = true,
+--       noremap = true,
+--       nowait = true,
+--     },
+--   },
 }
