374beaeedb3071672da3c101e3b59a9cdedd865b
Tung Pham <joseph.pham280996@gmail.com>
2025-01-01 02:51:45 -0500

    fix: Go back last tab still use old leader

--- lua/settings/keymappings.lua
+++ lua/settings/keymappings.lua
@@ -1,7 +1,11 @@
 local opts = { silent = true, noremap = true }
 
+vim.keymap.set("n", "<Space>", "<Nop>", opts)
+
+vim.g.mapleader = " "
+
 -- Let 'tl' toggle between this and the last accessed tab
-vim.api.nvim_set_keymap("n", "<Leader>utl", ':exe "tabn ".g:lasttab<CR>', opts)
+vim.api.nvim_set_keymap("n", "<leader>utl", ':exe "tabn ".g:lasttab<CR>', opts)
 vim.cmd([[ au TabLeave * let g:lasttab = tabpagenr() ]])
 
 -- {{ Split And Tabs }}
@@ -43,11 +47,6 @@ local normal_keymaps = {
     opts = vim.tbl_deep_extend("keep", opts, { desc = "Move Pane Border Downwards" }),
   },
 }
-
-vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
-
-vim.g.mapleader = " "
-
 -- Friendly Adjusting split size
 for mapping, cmd_opts in pairs(normal_keymaps) do
   vim.api.nvim_set_keymap("n", mapping, cmd_opts.cmd, cmd_opts.opts)
