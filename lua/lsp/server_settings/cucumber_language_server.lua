0643024174abbb600b34b64a15c8cb23e8c047dc
Tung Pham <joseph.pham280996@gmail.com>
2025-03-05 01:12:29 -0500

    fix(cucumber_language_server): Fix root_dir detection got deprecated

--- lua/lsp/server_settings/cucumber_language_server.lua
+++ lua/lsp/server_settings/cucumber_language_server.lua
@@ -4,5 +4,7 @@ return {
     ASDF_NODEJS_VERSION = "18.16.0",
   },
   cmd_cwd = nil,
-  root_dir = require("lspconfig.util").find_package_json_ancestor,
+  root_dir = vim.fs.dirname(
+    vim.fs.find({ "package.json", "package-lock.json", "node_modules" }, { upward = true })[1]
+  ),
 }
