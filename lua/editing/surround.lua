c4f6edcb09dab77a2b02918362ff2b499c5b90e2
Tung Pham <joseph.pham280996@gmail.com>
2025-03-08 00:35:25 -0500

    feat(surround): Consolidate all keymaps of surround

--- lua/editing/surround.lua
+++ lua/editing/surround.lua
@@ -1,78 +1,92 @@
--- Surround with quotes, brackets and paranthesis
+-- Surround with quotes, brackets, and parenthesis
 return {
   "kylechui/nvim-surround",
   version = "*",
-  opts = {
-    keymaps = {
-      insert = "<C-g>s",
-      insert_line = "<C-g>S",
-      visual = "S",
-      visual_line = "gS",
-    },
-  },
+  event = "VeryLazy",
+  config = true,
   keys = {
     {
-      "<leader>ss",
+      "<C-g>s",
+      "<Plug>(nvim-surround-insert)",
+      desc = "Surround normal",
+      mode = "i",
+      silent = true, -- use `silent` when creating keymaps
+      noremap = true, -- use `noremap` when creating keymaps
+      nowait = true, -- use `nowait` when creating keymaps
+    },
+    {
+      "<C-g>S",
+      "<Plug>(nvim-surround-insert-line)",
+      mode = "i",
+      desc = "Surround normal",
+      silent = true, -- use `silent` when creating keymaps
+      noremap = true, -- use `noremap` when creating keymaps
+      nowait = true, -- use `nowait` when creating keymaps
+    },
+    {
+      "s",
       "<Plug>(nvim-surround-normal)",
-      mode = "n",
-      desc = "Add a surrounding pair around a motion (normal mode)",
-      silent = true,
-      noremap = true,
-      nowait = true,
+      desc = "Surround normal",
+      silent = true, -- use `silent` when creating keymaps
+      noremap = true, -- use `noremap` when creating keymaps
+      nowait = true, -- use `nowait` when creating keymaps
     },
     {
-      "<leader>sc",
-      "<Plug>(nvim-surround-normal-cur)",
-      mode = "n",
-      desc = "Add a surrounding pair around the current line (normal mode)",
-      silent = true,
-      noremap = true,
-      nowait = true,
+      "S",
+      "<Plug>(nvim-surround-normal-curr)",
+      desc = "Surround normal current line",
+      silent = true, -- use `silent` when creating keymaps
+      noremap = true, -- use `noremap` when creating keymaps
+      nowait = true, -- use `nowait` when creating keymaps
     },
     {
-      "<leader>snl",
+      "yS",
       "<Plug>(nvim-surround-normal-line)",
-      mode = "n",
-      desc = "Add a surrounding pair around a motion, on new lines (normal mode)",
-      silent = true,
-      noremap = true,
-      nowait = true,
+      desc = "Surround normal delimiter on new line",
+      silent = true, -- use `silent` when creating keymaps
+      noremap = true, -- use `noremap` when creating keymaps
+      nowait = true, -- use `nowait` when creating keymaps
     },
     {
-      "<leader>sncl",
-      "<Plug>(nvim-surround-normal-cur-line)",
-      mode = "n",
-      desc = "Add a surrounding pair around a motion, on new lines (normal mode)",
-      silent = true,
-      noremap = true,
-      nowait = true,
+      "ySS",
+      "<Plug>(nvim-surround-normal-curr-line)",
+      desc = "Surround normal whole line delimiter on new line",
+      silent = true, -- use `silent` when creating keymaps
+      noremap = true, -- use `noremap` when creating keymaps
+      nowait = true, -- use `nowait` when creating keymaps
     },
     {
-      "<leader>suc",
-      "<Plug>(nvim-surround-change)",
-      mode = "n",
-      desc = "Change a surrounding pair",
-      silent = true,
-      noremap = true,
-      nowait = true,
+      "S",
+      "<Plug>(nvim-surround-visual)",
+      desc = "Surround visual",
+      mode = "x",
+      silent = true, -- use `silent` when creating keymaps
+      noremap = true, -- use `noremap` when creating keymaps
+      nowait = true, -- use `nowait` when creating keymaps
     },
     {
-      "<leader>sul",
-      "<Plug>(nvim-surround-change-line)",
-      mode = "n",
-      desc = "Change a surrounding pair, putting replacements on new lines",
-      silent = true,
-      noremap = true,
-      nowait = true,
+      "gS",
+      "<Plug>(nvim-surround-visual-line)",
+      desc = "Surround visual line",
+      silent = true, -- use `silent` when creating keymaps
+      noremap = true, -- use `noremap` when creating keymaps
+      nowait = true, -- use `nowait` when creating keymaps
     },
     {
-      "<leader>sd",
+      "ds",
       "<Plug>(nvim-surround-delete)",
-      mode = "n",
-      desc = "Delete a surrounding pair",
-      silent = true,
-      noremap = true,
-      nowait = true,
+      desc = "Delete surrounding",
+      silent = true, -- use `silent` when creating keymaps
+      noremap = true, -- use `noremap` when creating keymaps
+      nowait = true, -- use `nowait` when creating keymaps
+    },
+    {
+      "cS",
+      "<Plug>(nvim-surround-change-line)",
+      desc = "Change surrounding",
+      silent = true, -- use `silent` when creating keymaps
+      noremap = true, -- use `noremap` when creating keymaps
+      nowait = true, -- use `nowait` when creating keymaps
     },
   },
 }
