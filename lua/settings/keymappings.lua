bbc56506a815b5e7d479f1e2ff2916acfccc652a
Tung Pham <joseph.pham280996@gmail.com>
2025-03-11 00:24:24 -0400

    refactor(keyamppings): move all au to commands and options to options file + remove unused keymaps

--- lua/settings/keymappings.lua
+++ lua/settings/keymappings.lua
@@ -6,8 +6,6 @@ vim.g.mapleader = " "
 
 -- Let 'tl' toggle between this and the last accessed tab
 vim.api.nvim_set_keymap("n", "<leader>utl", ':exe "tabn ".g:lasttab<CR>', opts)
-vim.cmd([[ au TabLeave * let g:lasttab = tabpagenr() ]])
 
--- {{ Split And Tabs }}
-vim.opt.splitbelow = true
-vim.opt.splitright = true
+vim.cmd([[unmap gra]])
+vim.cmd([[unmap grn]])
