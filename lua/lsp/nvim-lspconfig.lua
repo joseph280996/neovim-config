3e024f41e810fcaf649a112849ce0a744c055851
Tung Pham <joseph.pham280996@gmail.com>
2025-04-02 14:10:23 -0400

    feat(fold): Auto detect whether lsp support fold and use it instead of treesitter

--- lua/lsp/nvim-lspconfig.lua
+++ lua/lsp/nvim-lspconfig.lua
@@ -26,6 +26,11 @@ return {
             client.server_capabilities.hoverProvider = false
           end
 
+          if client:supports_method("textDocument/foldingRange") then
+            local win = vim.api.nvim_get_current_win()
+            vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
+          end
+
           keymaps_setter(bufnr, lsp_keymaps)
         end,
         capabilities = require("lsp.config.capabilities"),
