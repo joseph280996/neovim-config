4daa7391fcea6e99d52c7e207a5e1f85eb1164fc
Tung Pham <joseph.pham280996@gmail.com>
2025-10-27 16:50:59 -0500

    feat(vimtex): Use sioyek to view pdf content

--- lua/document-tools/vimtex.lua
+++ lua/document-tools/vimtex.lua
@@ -11,43 +11,25 @@ local WSL_SETTING = {
   vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf",
 }
 
---@class VimtexViewerSettings
---@field vimtex_view_general_viewer string The path or program to execute for viewing of the rendered LaTeX.
---@field vimtex_view_general_options string The viewer options to run the viewer with.
-
---@param settings VimtexViewerSettings The settings to pass to vimtex viewer.
-local function set_vimtex_viewer(settings)
-  if settings then
-    vim.g.vimtex_view_general_viewer = settings.vimtex_view_general_viewer
-    vim.g.vimtex_view_general_options = settings.vimtex_view_general_options
-  end
-end
+local DARWIN_SETTING = {
+  vimtex_view_method = "sioyek",
+  vimtex_callback_progpath = "nvim",
+}
 
 return {
   -- LaTex
   "lervag/vimtex",
   ft = "tex",
   init = function()
-    -- This is necessary for VimTeX to load properly. The "indent" is optional.
-    -- Note that most plugin managers will do this automatically.
-    local options = {
-      filetype = on,
-      plugin = on,
-      indent = on,
-      syntax = enable,
-    }
-
-    for setting, value in pairs(options) do
-      vim.opt[setting] = value
-    end
-
     local vimtex_options = get_values_on_os({
       [constants.WINDOW] = WINDOW_SETTING,
       [constants.LINUX] = WSL_SETTING,
-      [constants.DARWIN] = nil,
+      [constants.DARWIN] = DARWIN_SETTING,
     })
 
-    set_vimtex_viewer(vimtex_options)
+    for setting, value in pairs(vimtex_options) do
+      vim.g[setting] = value
+    end
 
     vim.g.vimtex_compiler_method = "latexmk"
     vim.g.vimtex_complete_enabled = false
