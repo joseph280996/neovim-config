c4f83a17b23ce29ec668cb5aee6c9d9e34301f36
Pham, Tung <tung.pham@sscinc.com>
2026-01-20 09:19:43 -0600

    feat(snacks): Picker ignore files and add more projects

--- lua/editing/snacks/picker.lua
+++ lua/editing/snacks/picker.lua
@@ -1,4 +1,3 @@
-
 local constants = require("utils.constants")
 local get_values_on_os = require("utils.get-values-on-os")
 
@@ -15,15 +14,36 @@ return {
       layout = vim.tbl_deep_extend("keep", { preview = "main" }, custom_layout),
       filter = { cwd = true },
     },
+    files = {
+      hidden = true,
+      ignore = true,
+      exclude = { "**/*/node_modules/**", "**/*.git/**", "**/package-lock.json" },
+    },
+    grep = {
+      hidden = true,
+      ignore = true,
+      exclude = { "**/*/node_modules/**", "**/*.git/**", "**/package-lock.json" },
+    },
     project = {
-      dev = {
-        "~/.confg",
-        "~/Code/Personal/",
-        "~/Code/School/",
-        get_values_on_os({ [constants.WINDOW] = "D:/Notebook/", [constants.LINUX] = "/mnt/d/Notebook/" }),
-      },
+      dev = vim.list_extend(
+        {
+          "~/.confg",
+          "~/Code/Personal/",
+          "~/Code/Work/",
+        },
+        get_values_on_os({
+          [constants.WINDOW] = {
+            "D:/Notebook/",
+          },
+          [constants.LINUX] = { "/mnt/d/Notebook/" },
+          [constants.DARWIN] = {
+            "~/Documents/knowledge-base/",
+            "~/Documents/notes/",
+          },
+        }, false)
+      ),
       patterns = { ".git", "lazy-lock.json", "package.json", "Makefile", "README.md", ".zk" },
-      layout = custom_layout
+      layout = custom_layout,
     },
     lsp_definitions = {
       layout = custom_layout,
