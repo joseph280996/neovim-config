f9596a4f6e004d3211e78e66a8e698cecc475ee9
Pham, Tung <tung.pham@sscinc.com>
2026-02-08 20:26:39 -0600

    feat(no-neck-pain): add integration of aerial and neotest

--- lua/document-tools/no-neck-pain.lua
+++ lua/document-tools/no-neck-pain.lua
@@ -4,13 +4,27 @@ return {
   "shortcuts/no-neck-pain.nvim",
   version = "*",
   opts = {
-    width = 100,
+    width = 120,
     integrations = {
       -- @link https://github.com/nvim-neo-tree/neo-tree.nvim
       NeoTree = {
         -- The position of the tree.
         ---@type "left"|"right"
         position = "left",
+        -- When `true`, if the tree was opened before enabling the plugin, we will reopen it.
+        reopen = true,
+      },
+      aerial = {
+        -- The position of the tree.
+        ---@type "left"|"right"
+        position = "right",
+        -- When `true`, if the tree was opened before enabling the plugin, we will reopen it.
+        reopen = true,
+      },
+      neotest = {
+        -- The position of the tree.
+        ---@type "right"
+        position = "right",
         -- When `true`, if the tree was opened before enabling the plugin, we will reopen it.
         reopen = true,
       },
